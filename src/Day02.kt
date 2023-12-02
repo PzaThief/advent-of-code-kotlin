8c984ff8139cb1b30c2fa1384df1ce9e22dcb589
정민하 <vwjdalsgkv@gmail.com>
2023-12-02 19:23:13 +0900

    Solve day 02's part 2 problem

--- src/Day02.kt
+++ src/Day02.kt
@@ -4,6 +4,9 @@ fun main() {
 
     checkExample1()
     part1(givenCubeSet, input).println()
+
+    checkExample2()
+    part2(input).println()
 }
 
 private fun checkExample1() {
@@ -22,12 +25,32 @@ private fun checkExample1() {
 private fun part1(givenCubeSet: Game.CubeSet, input: List<String>): Int {
     val games = Game.parse(input)
     return games.sumOf {
-        val maximumCubes = it.getMaximumCubes()
-        if (maximumCubes.red <= givenCubeSet.red && maximumCubes.green <= givenCubeSet.green && maximumCubes.blue <= givenCubeSet.blue) {
+        val cubeSet = it.getFewestPossibleCubeSet()
+        if (cubeSet.red <= givenCubeSet.red && cubeSet.green <= givenCubeSet.green && cubeSet.blue <= givenCubeSet.blue) {
             it.id
         } else {
             0
         }
+    }
+}
+
+private fun checkExample2() {
+    val example = listOf(
+        "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+        "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+        "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+        "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+        "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
+    )
+    val expected = 2286
+    check(part2(example) == expected)
+}
+
+private fun part2(input: List<String>): Int {
+    val games = Game.parse(input)
+    return games.sumOf {
+        val maximumCubes = it.getFewestPossibleCubeSet()
+        maximumCubes.red * maximumCubes.green * maximumCubes.blue
     }
 }
 
@@ -75,7 +98,7 @@ private data class Game(
         }
     }
 
-    fun getMaximumCubes(): CubeSet {
+    fun getFewestPossibleCubeSet(): CubeSet {
         var (red, green, blue) = listOf(0, 0, 0)
         for (cube in this.sets) {
             red = maxOf(red, cube.red)
