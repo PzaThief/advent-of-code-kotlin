16c8303042415058798fb9caad4fee2a8b321a6d
PzaThief <vwjdalsgkv@gmail.com>
2023-12-03 22:13:48 +0900

    Solve day 03 part 2 problem

--- src/Day03.kt
+++ src/Day03.kt
@@ -4,8 +4,8 @@ fun main() {
     checkExample1()
     part1(input).println()
 
-//    checkExample2()
-//    part2(input).println()
+    checkExample2()
+    part2(input).println()
 }
 
 private fun checkExample1() {
@@ -38,7 +38,7 @@ private fun checkExample2() {
         "...$.*....",
         ".664.598.."
     )
-    val expected = 4361
+    val expected = 467835
     check(part2(example) == expected)
 }
 
@@ -48,15 +48,14 @@ private fun part1(input: List<String>): Int {
     for (i in input.indices) {
         for (j in input[i].indices) {
             if (input[i][j] != '.' && !input[i][j].isDigit()) {
-                markSet.add(Pair(i-1, j-1))
-                markSet.add(Pair(i-1, j))
-                markSet.add(Pair(i-1, j+1))
-                markSet.add(Pair(i, j-1))
-//                markSet.add(Pair(i, j)) // unnecessary
-                markSet.add(Pair(i, j+1))
-                markSet.add(Pair(i+1, j-1))
-                markSet.add(Pair(i+1, j))
-                markSet.add(Pair(i+1, j+1))
+                markSet.add(Pair(i - 1, j - 1))
+                markSet.add(Pair(i - 1, j))
+                markSet.add(Pair(i - 1, j + 1))
+                markSet.add(Pair(i, j - 1))
+                markSet.add(Pair(i, j + 1))
+                markSet.add(Pair(i + 1, j - 1))
+                markSet.add(Pair(i + 1, j))
+                markSet.add(Pair(i + 1, j + 1))
             }
         }
     }
@@ -71,7 +70,7 @@ private fun part1(input: List<String>): Int {
                     isValid = true
                 }
             }
-            if ((!input[i][j].isDigit() && stack.isNotEmpty()) || j == input[i].lastIndex){
+            if ((!input[i][j].isDigit() && stack.isNotEmpty()) || j == input[i].lastIndex) {
                 if (isValid) {
                     sum += stack.toInt()
                     isValid = false
@@ -86,5 +85,56 @@ private fun part1(input: List<String>): Int {
 }
 
 private fun part2(input: List<String>): Int {
-    return 0
+    var sum = 0
+    val markMap = mutableMapOf<Pair<Int, Int>, Pair<Int, Int>>()
+    for (i in input.indices) {
+        for (j in input[i].indices) {
+            if (input[i][j] == '*') {
+                val starPosition = Pair(i, j)
+                markMap[Pair(i - 1, j - 1)] = starPosition
+                markMap[Pair(i - 1, j)] = starPosition
+                markMap[Pair(i - 1, j + 1)] = starPosition
+                markMap[Pair(i, j - 1)] = starPosition
+                markMap[Pair(i, j + 1)] = starPosition
+                markMap[Pair(i + 1, j - 1)] = starPosition
+                markMap[Pair(i + 1, j)] = starPosition
+                markMap[Pair(i + 1, j + 1)] = starPosition
+            }
+        }
+    }
+
+    val numberByStar = mutableMapOf<Pair<Int, Int>, Pair<Int, Int>>()
+    for (i in input.indices) {
+        var stack = ""
+        var adjacentStar: Pair<Int, Int>? = null
+        for (j in input[i].indices) {
+            if (input[i][j].isDigit()) {
+                stack += input[i][j]
+                if (markMap.containsKey(Pair(i, j))) {
+                    adjacentStar = markMap[Pair(i, j)]
+                }
+            }
+            if ((!input[i][j].isDigit() && stack.isNotEmpty()) || j == input[i].lastIndex) {
+                if (adjacentStar != null) {
+                    numberByStar.compute(adjacentStar) { k, v ->
+                        if (v == null) {
+                            Pair(1, stack.toInt())
+                        } else {
+                            Pair(v.first + 1, v.second * stack.toInt())
+                        }
+                    }
+                    adjacentStar = null
+                }
+                stack = ""
+            }
+        }
+    }
+
+    for (possibleNumber in numberByStar.values) {
+        if (possibleNumber.first == 2) {
+            sum += possibleNumber.second
+        }
+    }
+
+    return sum
 }
