2dfe25ef281b2f5b691f8a635b1d6193039601f4
PzaThief <vwjdalsgkv@gmail.com>
2023-12-15 15:37:39 +0900

    Solve day 15 part 2 problem

--- src/Day15.kt
+++ src/Day15.kt
@@ -4,8 +4,8 @@ fun main() {
     checkExample1()
     part1(input).println()
 
-//    checkExample2()
-//    part2(input).println()
+    checkExample2()
+    part2(input).println()
 }
 
 private fun checkExample1() {
@@ -17,14 +17,41 @@ private fun checkExample1() {
 }
 
 private fun checkExample2() {
-
+    val example = listOf(
+        "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7",
+    )
+    val expected = 145L
+    check(part2(example) == expected)
 }
 
 private fun part1(input: List<String>): Long {
     val sequence = input.first().split(',')
-    return sequence.sumOf { it.fold(0) { acc, c -> (acc + c.code) * 17 % 256 }.toLong() }
+    return sequence.sumOf { hash(it).toLong() }
 }
 
 private fun part2(input: List<String>): Long {
-    return 0
+    val sequence = input.first().split(',')
+    val lenses = LinkedHashMap<String, Pair<Int, Int>>()
+    sequence.forEach {
+        if (it.last() == '-') {
+            val label = it.dropLast(1)
+            lenses.remove(label)
+        } else {
+            val label = it.dropLast(2)
+            val box = hash(label)
+            val focalLength = it.last().digitToInt()
+            lenses.compute(label) { _, lens ->
+                Pair(lens?.first ?: box, focalLength)
+            }
+        }
+    }
+    val slotByBox = mutableMapOf<Int, Int>()
+    return lenses.values.sumOf {
+        val slot = slotByBox.compute(it.first) { _, slot ->
+            if (slot == null) 1 else slot + 1
+        }!!
+        (it.first + 1) * slot * it.second.toLong()
+    }
 }
+
+private fun hash(str: String) = str.fold(0) { acc, c -> (acc + c.code) * 17 % 256 }
