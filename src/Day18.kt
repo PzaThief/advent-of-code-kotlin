4093d3d65e5b7b81571036bcff1a82cbf4fd4128
PzaThief <vwjdalsgkv@gmail.com>
2023-12-18 20:45:07 +0900

    Solve day 18 part 2 problem

--- src/Day18.kt
+++ src/Day18.kt
@@ -1,4 +1,4 @@
-import java.util.SortedSet
+import kotlin.math.abs
 
 fun main() {
     val input = readInput("Day18")
@@ -6,8 +6,8 @@ fun main() {
     checkExample1()
     part1(input).println()
 
-//    checkExample2()
-//    part2(input).println()
+    checkExample2()
+    part2(input).println()
 }
 
 private fun checkExample1() {
@@ -27,67 +27,80 @@ private fun checkExample1() {
         "L 2 (#015232)",
         "U 2 (#7a21e3)",
     )
-    val expected = 62
+    val expected = 62L
     check(part1(example) == expected)
 }
 
 private fun checkExample2() {
+    val example = listOf(
+        "R 6 (#70c710)",
+        "D 5 (#0dc571)",
+        "L 2 (#5713f0)",
+        "D 2 (#d2c081)",
+        "R 2 (#59c680)",
+        "D 2 (#411b91)",
+        "L 5 (#8ceee2)",
+        "U 2 (#caa173)",
+        "L 1 (#1b58a2)",
+        "U 2 (#caa171)",
+        "R 2 (#7807d2)",
+        "U 3 (#a77fa3)",
+        "L 2 (#015232)",
+        "U 2 (#7a21e3)",
+    )
+    val expected = 952408144115L
+    check(part2(example) == expected)
 }
 
-private fun part1(input: List<String>): Int {
+private fun part1(input: List<String>): Long {
     val lagoon = Day18Lagoon.parse(input)
     return lagoon.calculateLavaPool()
 }
 
-private fun part2(input: List<String>): Int {
-    return 0
+private fun part2(input: List<String>): Long {
+    val lagoon = Day18Lagoon.parse(input)
+    return lagoon.calculateLavaPool(true)
 }
 
 private class Day18Lagoon(val plans: List<Plan>) {
     enum class Direction {
-        U, L, D, R;
+        R, D, L, U;
 
-        fun move(pair: Pair<Int, Int>) = when (this) {
-            U -> Pair(pair.first - 1, pair.second)
-            L -> Pair(pair.first, pair.second - 1)
-            D -> Pair(pair.first + 1, pair.second)
-            R -> Pair(pair.first, pair.second + 1)
+        fun move(pair: Pair<Int, Int>, repeat: Int = 1) = when (this) {
+            U -> Pair(pair.first - repeat, pair.second)
+            L -> Pair(pair.first, pair.second - repeat)
+            D -> Pair(pair.first + repeat, pair.second)
+            R -> Pair(pair.first, pair.second + repeat)
         }
     }
 
-    data class Plan(val direction: Direction, val meters: Int, val color: String)
+    data class Plan(val direction: Direction, val meters: Int, val color: String) {
+        fun route(changePlan: Boolean): Pair<Int, Direction> = if (changePlan) {
+            Pair(color.take(5).toInt(16), Direction.entries[color.takeLast(1).toInt()])
+        } else {
+            Pair(meters, direction)
+        }
+    }
 
     companion object {
         fun parse(input: List<String>) = Day18Lagoon(input.map {
             val line = it.split(' ')
-            Plan(Direction.valueOf(line[0]), line[1].toInt(), line[2])
+            Plan(Direction.valueOf(line[0]), line[1].toInt(), line[2].removeSurrounding("(#", ")"))
         })
     }
 
-    fun calculateLavaPool(): Int {
-        val trenchByLine = sortedMapOf<Int, SortedSet<Int>>()
+    fun calculateLavaPool(changePlan: Boolean = false): Long {
         var currentPosition = Pair(0, 0)
-        trenchByLine.getOrPut(currentPosition.first) { sortedSetOf() }.add(currentPosition.second)
+        var outLine = 0L
+        var internalArea = 0L
         for (plan in plans) {
-            for (meter in 1..plan.meters) {
-                currentPosition = plan.direction.move(currentPosition)
-                trenchByLine.getOrPut(currentPosition.first) { sortedSetOf() }.add(currentPosition.second)
+            val (meters, direction) = plan.route(changePlan)
+            currentPosition = direction.move(currentPosition, meters).also { nextPosition ->
+                outLine += meters
+                internalArea += (nextPosition.second - currentPosition.second) * (nextPosition.first + currentPosition.first).toLong()
             }
         }
 
-        var lavaPool = 0
-        val leftTop = Pair(trenchByLine.firstKey(), trenchByLine[trenchByLine.firstKey()]!!.first())
-        val queue = mutableListOf(Pair(leftTop.first + 1, leftTop.second + 1))
-        val visited = mutableSetOf(queue.first())
-        while (queue.isNotEmpty()) {
-            val interior = queue.removeFirst()
-            lavaPool++
-            Direction.entries.forEach {
-                val near = it.move(interior)
-                if (near.second !in trenchByLine[near.first]!!) visited.add(near) && queue.add(near)
-            }
-        }
-        lavaPool += trenchByLine.values.sumOf { it.count() }
-        return lavaPool
+        return abs(internalArea / 2) + outLine / 2 + 1
     }
 }
