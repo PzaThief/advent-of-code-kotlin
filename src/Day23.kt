44c9fb8b76c88fdf290bd7cf6a33f593db1aea3a
정민하 <vwjdalsgkv@gmail.com>
2023-12-24 01:07:43 +0900

    Solve day 23 part 2 problem

--- src/Day23.kt
+++ src/Day23.kt
@@ -1,4 +1,3 @@
-import java.util.*
 import kotlin.math.abs
 
 fun main() {
@@ -7,8 +6,8 @@ fun main() {
     checkExample1()
     part1(input).println()
 
-//    checkExample2()
-//    part2(input).println()
+    checkExample2()
+    part2(input).println()
 }
 
 private fun checkExample1() {
@@ -42,20 +41,52 @@ private fun checkExample1() {
 }
 
 private fun checkExample2() {
+    val example = listOf(
+        "#.#####################",
+        "#.......#########...###",
+        "#######.#########.#.###",
+        "###.....#.>.>.###.#.###",
+        "###v#####.#v#.###.#.###",
+        "###.>...#.#.#.....#...#",
+        "###v###.#.#.#########.#",
+        "###...#.#.#.......#...#",
+        "#####.#.#.#######.#.###",
+        "#.....#.#.#.......#...#",
+        "#.#####.#.#.#########v#",
+        "#.#...#...#...###...>.#",
+        "#.#.#v#######v###.###v#",
+        "#...#.>.#...>.>.#.###.#",
+        "#####v#.#.###v#.#.###.#",
+        "#.....#...#...#.#.#...#",
+        "#.#########.###.#.#.###",
+        "#...###...#...#...#.###",
+        "###.###.#.###v#####v###",
+        "#...#...#.#.>.>.#.>.###",
+        "#.###.###.#.###.#.#v###",
+        "#.....###...###...#...#",
+        "#####################.#",
+    )
+    val expected = 154L
+    check(part2(example) == expected)
 }
 
 private fun part1(input: List<String>): Long {
     val system = Day23Map(input)
-    return system.getPossibleSteps().max().toLong()
+    return system.getLongestWay().toLong()
 }
 
 private fun part2(input: List<String>): Long {
-    return 0
+    val system = Day23Map(input)
+    return system.getLongestWay(true).toLong()
 }
 
 private class Day23Map(val pattern: List<String>) {
-    val start = 0 to pattern.first().indexOf('.')
-    val end = pattern.lastIndex to pattern.last().indexOf('.')
+    val hikeStart = 0 to pattern.first().indexOf('.')
+    val hikeEnd = pattern.lastIndex to pattern.last().indexOf('.')
+    val junctions = mutableMapOf(
+        hikeStart to mutableListOf<Pair<Pair<Int, Int>, Int>>(),
+        hikeEnd to mutableListOf(),
+    )
 
     enum class Direction(val slope: Char) {
         U('^'), L('<'), D('v'), R('>');
@@ -86,34 +117,72 @@ private class Day23Map(val pattern: List<String>) {
         }
     }
 
-    fun getPossibleSteps(): List<Int> {
-        val steps = mutableListOf<Int>()
-        val pathQueue = LinkedList<LinkedHashSet<Pair<Int, Int>>>()
-        pathQueue.add(linkedSetOf(start))
-        while (pathQueue.isNotEmpty()) {
-            val currentPath = pathQueue.pop()
-            val currentPoint = currentPath.last()
-            if (currentPoint == end) {
-                steps.add(currentPath.count() - 1)
-                continue
-            }
-            val nextPoints = Direction.entries
-                .map { it to it.move(currentPoint) }
-                .filter { (direction, next) ->
-                    if (next.first !in 0..pattern.lastIndex || next.second !in 0..pattern.last().lastIndex) return@filter false
-                    if (next in currentPath) return@filter false
-                    val char = pattern[next.first][next.second]
-                    val flag = Flag.from(char)
-                    if (flag == Flag.FOREST) return@filter false
-                    if (flag == Flag.SLOPE) {
-                        if (Direction.from(char)!!.isOpposite(direction)) return@filter false
+    fun getLongestWay(useCrampon: Boolean = false): Int {
+        for (row in pattern.indices) {
+            for (col in pattern[row].indices) {
+                if (pattern[row][col] == '.') {
+                    val point = Pair(row, col)
+                    if (validNearPoints(point, true).size > 2) {
+                        junctions[point] = mutableListOf()
                     }
-                    return@filter true
                 }
-            pathQueue.addAll(nextPoints.map { currentPath.plus(it.second) as LinkedHashSet })
+            }
         }
 
-        return steps.also { println(it) }
+        for (junction in junctions.keys) {
+            var current = setOf(junction)
+            val visited = mutableSetOf(junction)
+            var distance = 0
+
+            while (current.isNotEmpty()) {
+                distance++
+                current = buildSet {
+                    for (c in current) {
+                        validNearPoints(c, useCrampon).filter { it !in visited }.forEach { near ->
+                            if (near in junctions) {
+                                junctions.getValue(junction).add(near to distance)
+                            } else {
+                                add(near)
+                                visited.add(near)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return largestStepFrom(current = hikeStart)
     }
 
+    private fun validNearPoints(start: Pair<Int, Int>, useCrampon: Boolean): List<Pair<Int, Int>> {
+        return Direction.entries
+            .map { it to it.move(start) }
+            .filter { (direction, next) ->
+                if (next.first !in 0..pattern.lastIndex || next.second !in 0..pattern.last().lastIndex) return@filter false
+                val char = pattern[next.first][next.second]
+                val flag = Flag.from(char)
+                if (flag == Flag.FOREST) return@filter false
+                if (flag == Flag.SLOPE) {
+                    if (!useCrampon && Direction.from(char)!!.isOpposite(direction)) return@filter false
+                }
+                return@filter true
+            }.map { it.second }
+    }
+
+    private fun largestStepFrom(
+        current: Pair<Int, Int>,
+        path: Set<Pair<Int, Int>> = setOf(),
+        baseDistance: Int = 0,
+    ): Int {
+        if (current == hikeEnd) return baseDistance
+
+        val newPath = path.plus(current) as LinkedHashSet
+        val max = junctions.getValue(current)
+            .filter { (nextJunction) -> nextJunction !in newPath }
+            .maxOfOrNull { (nextJunction, distance) ->
+                largestStepFrom(nextJunction, newPath, baseDistance + distance)
+            }
+
+        return max ?: 0
+    }
 }
